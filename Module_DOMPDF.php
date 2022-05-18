d471fb04a44b7cc61322df5822a5c338e7350376
gizmore <gizmore@wechall.net>
2022-05-18 22:31:47 +0200

    Init

---
+++ Module_DOMPDF.php
@@ -0,0 +1,16 @@
+<?php
+namespace GDO\DOMPDF;
+
+use GDO\Core\GDO_Module;
+
+/**
+ * DOMPDF library wrapper.
+ * 
+ * @author gizmore
+ * @version 7.0.0
+ * @since 7.0.0
+ */
+final class Module_DOMPDF extends GDO_Module
+{
+	
+}
